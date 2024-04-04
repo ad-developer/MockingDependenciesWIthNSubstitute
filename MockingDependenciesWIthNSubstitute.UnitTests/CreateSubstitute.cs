6805dacf78e68e4e0403e1e9c5b8fd93c96820c2
Alexandr Dolenko <ad@Alexandrs-MBP.lan>
2024-04-04 10:07:36 -0700

    increment

---
+++ MockingDependenciesWIthNSubstitute.UnitTests/CreateSubstitute.cs
@@ -0,0 +1,76 @@
+﻿using MockingDependenciesWIthNSubstitute.Application;
+using MockingDependenciesWIthNSubstitute.Application.Interfaces;
+using NSubstitute;
+
+namespace MockingDependenciesWIthNSubstitute.UnitTests;
+
+public class CreateSubstitute
+{
+
+   [Fact] 
+    public void SubstituteTest(){
+        
+        // Arrange
+
+        var  legacySystemProxy = Substitute.For<ILegacySystemProxy>();
+        var customerValidationService = Substitute.For<ICustomerValidationService>();
+        var accountValidationService = Substitute.For<IAccountValidationService>();
+
+        var sut = new AccountService(legacySystemProxy, customerValidationService, accountValidationService);
+        var customer = new Customer{
+            FirstName = "John",
+            LastName = "Smith",
+            Address = "Some street",
+            Email = "John.Smith@email.com",
+            PhoneNumber = "(333) 333 - 4444"
+        };
+
+        // Act 
+        var ex = Record.Exception(() => {
+            sut.CreateAccount(customer);
+        });
+
+        // Accert
+        Assert.NotNull(ex); 
+    }
+
+    [Fact]
+     public void MultipleSubstituteTest(){
+        
+        // Arrange
+
+        var  legacySystemProxy = Substitute.For<ILegacySystemProxy, IDisposable>();
+        var customerValidationService = Substitute.For<ICustomerValidationService>();
+        var accountValidationService = Substitute.For<IAccountValidationService>();
+
+        var sut = new AccountService(legacySystemProxy, customerValidationService, accountValidationService);
+        var customer = new Customer{
+            FirstName = "John",
+            LastName = "Smith",
+            Address = "Some street",
+            Email = "John.Smith@email.com",
+            PhoneNumber = "(333) 333 - 4444"
+        };
+
+        // Act 
+        var ex = Record.Exception(() => {
+            sut.CreateAccount(customer);
+        });
+
+        // Accert
+        Assert.NotNull(ex); 
+    }
+
+     [Fact]
+     public void SubstituteForDelegatesTest(){
+        
+        // Arrange
+        var func = Substitute.For<Func<string>>();
+        
+        // Act
+        func().Returns("hello");
+        
+        // Assert
+        Assert.Equal("hello", func());
+    }
+}
